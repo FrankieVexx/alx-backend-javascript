9e9cc3636f97755fdfe1070cc75df439181c52ef
Frankie Vexx <franblog7@gmail.com>
2023-08-24 16:21:08 +0300

    writing tests with sinon

---
+++ 0x06-unittests_in_js/3-payment.js
@@ -0,0 +1,7 @@
+const utils = require('./utils');
+
+const sendPaymentRequestToApi = (totalAmount, totalShipping) => {
+    const totals = utils.calculateNumber('SUM', totalAmount, totalShipping);
+    console.log(`The total is: ${totals}`);
+    return totals;
+    }
